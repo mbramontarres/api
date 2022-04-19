c9c45093fbf896f06889cfcb3ce31c61df5e3e6c
Marc Bramón Tarrés <mbramon@epi.cat>
2022-04-19 16:51:05 +0200

    easy api created

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    CatModule,
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
