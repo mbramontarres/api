139277ae85b6ab5633b94bedff06e0b142c07c03
marc <marcbt@gmail.com>
2022-05-09 22:10:43 +0200

    paralel

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -15,7 +15,7 @@ import { TransferSchema } from "../src/transfer/transfer.schema";
 import { TransferType } from "../src/transfer/dto/transfer.dto";
 import { AccountSchema } from "../src/account/account.schema";
 import { AccountType } from "../src/account/dto/account.dto";
-import { addLogs, getAccountProperies, processBlockData } from "./helpers/blockData";
+import { addLogs, addOrReplaceAccount, getAccountProperies, processBlockData } from "./helpers/blockData";
 
 async function Run(){
     const wsProvider = new WsProvider(config.wsProviderUrl);
@@ -29,27 +29,40 @@ async function Run(){
             console.log("Couldn't connect to MongoDb Database",error);
         }
     );
+    //Almenys un block a la col·lecció
+    const buit = await db.model('blocks', BlockSchema).find()
+    if(buit.length==0){
+      console.log("primer block");
+      const last = await api.rpc.chain.getHeader();
+      await addBlocksDb(db,api,last.number);
+    }
 
-    //Promise.all(findGaps(db,api,null), processAllAccounts(api))
+    Promise.all([ findGaps(db,api,null),  processAllAccounts(api,db),  listenBlocks(api,db)])
     //findGaps(db,api,null);
     //await processAllAccounts(api);
-    const chain = await api.rpc.system.chain();
-    await api.rpc.chain.subscribeNewHeads(async (lastHeader) => {
-        console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
-
-       await addBlocksDb(db,api,lastHeader.number.toNumber());
-
-    });
+    
 
 }
 
-async function processAllAccounts(api) {
+async function listenBlocks(api,db){
+  const chain = await api.rpc.system.chain();
+  await api.rpc.chain.subscribeNewHeads(async (lastHeader) => {
+      console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
+
+     await addBlocksDb(db,api,lastHeader.number.toNumber());
+
+  });
+}
+
+async function processAllAccounts(api,db) {
+  console.log("Thread Accounts Started")
   const accounts = await api.query.system.account.keys();
   const accountsIds = accounts.map(({ args }) => args).map(([e]) => e.toHuman());
   //parallize
-
+  console.log("Thread Accounts: accounts trobades.")
   accountsIds.forEach(async a=> {
-    const acc  = await getAccountProperies(api,a);
+    await addOrReplaceAccount(api,a,db);
+    //const acc  = await getAccountProperies(api,a);
     //Afegir si no existeix;
     
   });
@@ -57,6 +70,7 @@ async function processAllAccounts(api) {
 }
 
 async function findGaps(db,api: ApiPromise, lastHeader){
+  console.log("Gaps Thread Started")
     const gaps =  await db.model('blocks', BlockSchema).aggregate([
         {
           $unset: [
@@ -162,9 +176,14 @@ async function findGaps(db,api: ApiPromise, lastHeader){
       ])
       
     console.log(gaps);
-    /*gaps.forEach(async gap => {
-         //await addBlocksDb(db,api,gap);
-    });*/
+    gaps.forEach(async gap => {
+      let block = gap.left;
+        while(block<=gap.right){
+          await addBlocksDb(db,api,block);
+          block++;
+        }
+        console.log("Gaps Thread Started:Gap afegit")
+    });
 }
 
 async function addBlocksDb(db,api: ApiPromise,blockNum) {
@@ -201,7 +220,7 @@ async function addBlocksDb(db,api: ApiPromise,blockNum) {
      block.parentHash = CurrentBlock.block.header.parentHash.toHex();
      block.extrinsicsRoot = CurrentBlock.block.header.extrinsicsRoot.toHex();
      block.stateRoot = CurrentBlock.block.header.stateRoot.toHex();
-     block.blockAuthor = extendedBlock.author.toString();
+     block.blockAuthor = extendedBlock.author? extendedBlock.author.toString():"";
      //treure counts
      block.eventCount = extendedBlock.events.length;
      block.extrinsicsCount = extendedBlock.block.extrinsics.length;
